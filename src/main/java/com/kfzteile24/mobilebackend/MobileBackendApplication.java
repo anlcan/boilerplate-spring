a654b9841f7924310b0c8b0a7b4858e992e74843
Anil Can Baykal <anilcan.baykal@kfzteile24.de>
2024-01-03 12:23:28 +0100

    spring initializr

---
+++ src/main/java/com/kfzteile24/mobilebackend/MobileBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kfzteile24.mobilebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileBackendApplication.class, args);
+	}
+
+}
